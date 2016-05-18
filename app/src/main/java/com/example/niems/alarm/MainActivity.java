e09a7a9565551a61bc19014920d93f4f2c88d892
niems <czniemann1990@gmail.com>
2016-05-18 00:23:32 -0500

    added error checking so it doesn't crash. Dialog buttons still throw errors

--- app/src/main/java/com/example/niems/alarm/MainActivity.java
+++ app/src/main/java/com/example/niems/alarm/MainActivity.java
@@ -9,11 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //used to test the functionality of saving from the dialog
+    //and
+    private String test_word;
+    private String test_word_definition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        this.test_word = "";
+        this.test_word_definition = "";
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -22,9 +31,15 @@ public class MainActivity extends AppCompatActivity {
     public void addWord( View view ){
 
         //brings up the dialog box to add a new word
-        Dialog new_word = new Dialog(this);
-        new_word.setContentView( R.layout.add_word_dialog );
-        new_word.show();
+        try{
+            Dialog new_word = new Dialog(this);
+            new_word.setContentView( R.layout.add_word_dialog );
+            new_word.show();
+
+        }catch(Exception e){
+            Toast.makeText(this, "Error: addWord()", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 
@@ -34,18 +49,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void dialogClear( View view ){ //user clears the new word and definition
 
-        if( view.isShown() ){
-            EditText new_word = (EditText) findViewById( R.id.new_word );
-            EditText new_word_definition = (EditText) findViewById( R.id.new_word_definition );
+        try {
+            EditText new_word = (EditText) findViewById(R.id.new_word);
+            EditText new_word_definition = (EditText) findViewById(R.id.new_word_definition);
 
             //resets new word and definition
-            new_word.setText( R.string.new_word );
-            new_word_definition.setText( R.string.new_word_definition );
+            new_word.setText(R.string.new_word);
+            new_word_definition.setText(R.string.new_word_definition);
+
+        }catch(Exception e){
+            Toast.makeText(this, "Error: dialogClear()", Toast.LENGTH_SHORT).show();
         }
+
 
     }
 
     public void dialogSave( View view ){ //user saves the new word and definition
+        try{
+            this.test_word = findViewById( R.id.new_word ).toString();
+            this.test_word_definition = findViewById( R.id.new_word_definition ).toString();
 
+        }catch(Exception e){
+            Toast.makeText(this, "Error: dialogSave()", Toast.LENGTH_SHORT).show();
+        }
     }
 }
