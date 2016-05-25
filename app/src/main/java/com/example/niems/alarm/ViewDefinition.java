24c327531ab087258535f6cefabb21e61b049cb5
niems <czniemann1990@gmail.com>
2016-05-25 18:37:09 -0500

    viewDefinition now uses a function to find the index for the word the user clicked

--- app/src/main/java/com/example/niems/alarm/ViewDefinition.java
+++ app/src/main/java/com/example/niems/alarm/ViewDefinition.java
@@ -3,10 +3,16 @@ package com.example.niems.alarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewDefinition extends AppCompatActivity {
+    public static final String view_word = "display word";
+    public static final String view_def = "display definition";
+
+    private String current_view;
+    private int word_index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,30 +23,67 @@ public class ViewDefinition extends AppCompatActivity {
         setSupportActionBar( toolbar );
         toolbar.setTitle("Words");
 
+        current_view = "display definition";
+
         try{
             //modifies the current view
             TextView current_word = (TextView) findViewById( R.id.view_word );
             TextView current_word_def = (TextView) findViewById( R.id.view_word_def );
-            String word = "";
-            String word_def = "";
-            int word_index = 0; //used to find the index of the word the user pressed
+            String word;      //temporary storage for current word
+            String word_def; //temporary storage for current word definition
 
-            for(; word_index < MainActivity.word_collection.size(); word_index++){
+            if( findWordIndex() ){ //if the current word is found
+                word = MainActivity.word_collection.get( this.word_index ).getWord();
+                word_def = MainActivity.word_collection.get( this.word_index ).getWordDef();
 
-                if(MainActivity.word_collection.get( word_index ).getWord().equals( MainActivity.word_selected ) ){
-                    word = MainActivity.word_collection.get( word_index ).getWord();
-                    word_def = MainActivity.word_collection.get( word_index ).getWordDef();
-
-                    break; //word found
-                }
+                current_word.setText( word );
+                current_word_def.setText( word_def );
+            }
+            else{
+                Toast.makeText(this, "Word not found", Toast.LENGTH_SHORT).show();
             }
 
-            current_word.setText( word );
-            current_word_def.setText( word_def );
 
         }catch(Exception e){
             Toast.makeText(this, "Error: ViewDefintion - onCreate()", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    public boolean findWordIndex(){ //returns the current word index if found, or -1
+        boolean found_index = false;
+
+        for(int i = 0; i < MainActivity.word_collection.size(); i++) {
+
+            if (MainActivity.word_collection.get(i).getWord().equals(MainActivity.word_selected)) {
+                found_index = true; //the word has been found
+                this.word_index = i; //index for the current word
+                break; //word found
+            }
+        }
+
+        return found_index;
+    }
+
+    public void toggleView( View view ){
+
+        try{
+            TextView current_word = (TextView) findViewById( R.id.view_word );
+            TextView current_word_def = (TextView) findViewById( R.id.view_word_def );
+
+            if(current_view.equals( view_word ) ){ //the word & def needs to be displayed
+
+            }
+
+            else if(current_view.equals( view_def ) ){ //the word needs to be displayed
+
+            }
+
+            else{
+                Toast.makeText(this, "Error: View Defintion - toggleView()", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch(Exception e){
+            Toast.makeText(this, "Error: ViewDefinition - toggleView()", Toast.LENGTH_SHORT).show();
+        }
     }
 }
